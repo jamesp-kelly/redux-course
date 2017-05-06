30076c3da2661825e3b289d1ea0d56bb2d137026
James Kelly <jk_jameskelly@outlook.com>
2017-05-06 10:44:06 -0400

    load and save state from localstorage

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import todoApp from './reducers';
 import { TodoApp } from './components';
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle';
 
+const persistedState = loadState();
+
+const store = createStore(todoApp, persistedState);
+
+store.subscribe(throttle(() => {
+  saveState({
+    todos: store.getState().todos
+  });
+}), 1000);
 
 ReactDOM.render(
-  <Provider store={createStore(todoApp)}>
+  <Provider store={store}>
     <TodoApp />
   </Provider>,
   document.getElementById('root')
